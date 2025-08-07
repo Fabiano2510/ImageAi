f4d3238b28df4005306c04941ff243690b9ae0c3
Fabiano2510 <131485651+Fabiano2510@users.noreply.github.com>
2025-08-06 22:05:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import requests
+from diffusers import DiffusionPipeline
+import torch
 
-app = FastAPI()
+model_name = "Qwen/Qwen-Image"
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# Load the pipeline
+if torch.cuda.is_available():
+    torch_dtype = torch.bfloat16
+    device = "cuda"
+else:
+    torch_dtype = torch.float32
+    device = "cpu"
 
-class PromptRequest(BaseModel):
-    prompt: str
+pipe = DiffusionPipeline.from_pretrained(model_name, torch_dtype=torch_dtype)
+pipe = pipe.to(device)
 
-DEEP_AI_API_KEY = "81d53499-be78-4580-8f73-b542389ff0b3"  # Consíguela gratis en https://deepai.org
+positive_magic = {
+    "en": "Ultra HD, 4K, cinematic composition.", # for english prompt
+    "zh": "超清，4K，电影级构图", # for chinese prompt
+}
 
-@app.post("/generate-image")
-async def generate_image(data: PromptRequest):
-    response = requests.post(
-        "https://api.deepai.org/api/stable-diffusion",
-        data={"text": data.prompt},
-        headers={"api-key": DEEP_AI_API_KEY}
-    )
+# Generate image
+prompt = '''A coffee shop entrance features a chalkboard sign reading "Qwen Coffee 😊 $2 per cup," with a neon light beside it displaying "通义千问". Next to it hangs a poster showing a beautiful Chinese woman, and beneath the poster is written "π≈3.1415926-53589793-23846264-33832795-02384197". Ultra HD, 4K, cinematic composition'''
 
-    if response.status_code == 200:
-        output = response.json()
-        return {"success": True, "image_url": output["output_url"]}
-    else:
-        return {
-            "success": False,
-            "status": response.status_code,
-            "error": response.text
-        }
+negative_prompt = " " # using an empty string if you do not have specific concept to remove
+
+
+# Generate with different aspect ratios
+aspect_ratios = {
+    "1:1": (1328, 1328),
+    "16:9": (1664, 928),
+    "9:16": (928, 1664),
+    "4:3": (1472, 1140),
+    "3:4": (1140, 1472),
+    "3:2": (1584, 1056),
+    "2:3": (1056, 1584),
+}
+
+width, height = aspect_ratios["16:9"]
+
+image = pipe(
+    prompt=prompt + positive_magic["en"],
+    negative_prompt=negative_prompt,
+    width=width,
+    height=height,
+    num_inference_steps=50,
+    true_cfg_scale=4.0,
+    generator=torch.Generator(device="cuda").manual_seed(42)
+).images[0]
+
+image.save("example.png")
